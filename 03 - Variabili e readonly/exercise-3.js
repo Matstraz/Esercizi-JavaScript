c31cf22d46d6a2b48c2be9ee5e85af8f9b7c1a63
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-06 02:46:52 +0200

    ex 3

--- 03 - Variabili e readonly/exercise-3.js
+++ 03 - Variabili e readonly/exercise-3.js
@@ -1,8 +1,13 @@
 const students = ['Paul', 'George', 'Lucas'];
 
 function addStudent(student) {
-  students.push('Marco');
+  students.push(student)
 }
 
 addStudent('Marco');
 console.log(students);
+
+/*Sarà possibile aggiungere un elemento all'array nonostante la variabile
+sia readonly poichè quest'ultima non verrà in alcun modo riassegnata, farà
+sempre riferimento al medesimo array. Ad ogni modo, è eventualmente 
+ammissibile riassegnare una costante in local scope.*/
