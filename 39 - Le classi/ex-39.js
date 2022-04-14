e84ff0fbf381c688cc38abd1bfcee1608c6642c6
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-14 17:52:27 +0200

    ex 39

--- 39 - Le classi/ex-39.js
+++ 39 - Le classi/ex-39.js
@@ -18,7 +18,23 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  constructor(width, height, radius){
+    this.width = width;
+    this.height = height;
+    this.radius = radius;
+  }
+  
+  static calculate(figure){
+    if(figure.side){
+      return figure.side**2 ;
+    }else if(figure.height){
+      return figure.height * figure.width;
+    }else if(figure.radius){
+      return figure.radius**2 * Math.PI
+    }else{
+      return `I don't know this figure!` 
+    }
+  }
 }
 
 const square = new Square(4);
