84d9f4928c1029695c0a54ef39fb28bfd2caf079
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-14 16:16:41 +0200

    ex-37.js

--- 37 - Le classi/ex-37.js
+++ 37 - Le classi/ex-37.js
@@ -10,13 +10,13 @@ class BankAccount{
         return this.initialFounds -= subtract
     }
     view(){
-        return this.initialFounds
+        console.log(this.initialFounds) 
     }
 }
 
 
 const bankAccount = new BankAccount(1000);
-console.log(bankAccount.deposit(500));
-console.log(bankAccount.deposit(200));
-console.log(bankAccount.withdraw(800));
-console.log(bankAccount.view());
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(800);
+bankAccount.view();
