bc437add90f00ec6b3dc8ab54ea586e5fd02d3c0
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-15 02:57:11 +0200

    ex 47

--- 47 - Decostructiong assignment/ex-47.js
+++ 47 - Decostructiong assignment/ex-47.js
@@ -8,3 +8,8 @@ const person = {
 for (let item of Object.entries(person)) {
   console.log(`${item[0]}: ${item[1]}`);
 }
+
+
+for (let [key, value] of Object.entries(person)) {
+  console.log(`${key}: ${value}`);
+}
