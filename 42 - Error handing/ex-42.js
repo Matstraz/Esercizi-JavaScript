200d3e930327bf95ac8c8c2bc5321a5177b14dca
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-19 02:46:48 +0200

    ex 42

--- 42 - Error handing/ex-42.js
+++ 42 - Error handing/ex-42.js
@@ -14,12 +14,16 @@ class BankAccount {
   }
 
   withdraw(amount) {
+    try{
     if (amount < 0) {
       throw new Error('The amount provided cannot be negative');
     }
 
     if (this.#amount < amount) {
       throw new Error('You cannot withdraw more than account balance');
+    }
+    }catch(err){
+      console.log(err)
     }
 
     this.#amount -= amount;
