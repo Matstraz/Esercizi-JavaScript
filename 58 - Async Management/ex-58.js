d78d879533f003a98287e96e8f2f0d5f9183b588
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-23 16:38:24 +0200

    ex 58

--- 58 - Async Management/ex-58.js
+++ 58 - Async Management/ex-58.js
@@ -22,8 +22,20 @@ const persons = [
 
 function fetchPersonById(id) {
   return new Promise((resolve, reject) => {
+    setTimeout(() => {                                                  //Altre due possibile alternative funzionanti per soddisfare la condizione
+      if(typeof persons.find(item => item.id === id) !== 'undefined'){  //1) if(Boolean(persons.find(item => item.id === id)) == true)
+        resolve(persons.find(item => item.id === id))                   //2) if(persons.find(item => item.id === id))
+      }else{
+        reject(new Error('No names with this id'))
+      }
+      }, 1000);
+  });
+}
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(typeof persons.find(item => item.id === id) !== 'undefined'){
+      if(Boolean(persons.find(item => item.id === id)) == true){
         resolve(persons.find(item => item.id === id))
       }else{
         reject(new Error('No names with this id'))
