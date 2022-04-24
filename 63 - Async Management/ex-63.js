4b81db75ab45be64f8f76e4d7dffd371ce3b2e7e
Matteo <99515747+Matstraz@users.noreply.github.com>
2022-04-24 19:26:10 +0200

    ex 63

--- 63 - Async Management/ex-63.js
+++ 63 - Async Management/ex-63.js
@@ -33,7 +33,12 @@ function fetchPersonById(id) {
   });
 }
 
-fetchPersonById(2)
-  .then((personJson) => JSON.parse(personJson))
-  .then((person) => console.log(person))
-  .catch((err) => console.error(err));
+(async () => {
+  try {
+    const personJson = await fetchPersonById(4);
+    const person = await JSON.parse(personJson);
+    console.log(person);
+  }catch(err){
+    console.log(err)
+  };
+})()
